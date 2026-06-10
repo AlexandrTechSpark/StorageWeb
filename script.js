dc381102e91c09330dfec9f63e6759ea914c8c42
Alexandr <luse.off@gmail.com>
2026-06-10 19:18:17 +0300

    init

---
+++ script.js
@@ -0,0 +1,64 @@
+const APPS_SCRIPT_URL =
+    "https://script.google.com/macros/s/AKfycbzprO3pp1DOWD3mAkpyIrgO1BK2EBm8wqshg38ZFotTo0olWlMY94DgUUgkNYNOhYFWQg/exec";
+	
+
+function extractPart(text)
+{
+    let match = text.match(/Part=([^;]+)/i);
+
+    if (match)
+        return match[1].trim();
+
+    match = text.match(/Component=([^;]+)/i);
+
+    if (match)
+        return match[1].trim();
+
+    return null;
+}
+
+async function sendPart(part)
+{
+    try
+    {
+        const response = await fetch(
+            APPS_SCRIPT_URL +
+            "?part=" +
+            encodeURIComponent(part)
+        );
+
+        const result = await response.text();
+
+        document.getElementById("status").innerHTML =
+            "? Збережено: " + part;
+    }
+    catch(error)
+    {
+        document.getElementById("status").innerHTML =
+            "? Помилка відправки";
+    }
+}
+
+function onScanSuccess(decodedText)
+{
+    const part = extractPart(decodedText);
+
+    if (!part)
+    {
+        document.getElementById("status").innerHTML =
+            "Не знайдено поле Part";
+        return;
+    }
+
+    sendPart(part);
+}
+
+const scanner = new Html5QrcodeScanner(
+    "reader",
+    {
+        fps: 10,
+        qrbox: 250
+    }
+);
+
+scanner.render(onScanSuccess);
